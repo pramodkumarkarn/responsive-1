b9394f84c294e66380546c4d0ed829b023e6eb71
NovikovViktor <viktor.novikov95@gmail.com>
2015-06-07 15:20:38 +0300

    Add some styles for toggling menu for small-screen devices

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ document.getElementById('nav-button').onclick = function (event) {
     event.preventDefault();
     var nav_panel = document.getElementById('menu-nav').getElementsByClassName('nav')[0];
     if (nav_panel.className.indexOf('visible-menu') > -1) {
-        nav_panel.style.display= 'none';
+        nav_panel.style.display = 'none';
         nav_panel.className = nav_panel.className.replace(' visible-menu', '');
     } else {
         nav_panel.className = nav_panel.className + ' visible-menu';
-        nav_panel.style.display= 'block';
+        nav_panel.style.display = 'block';
     }
 };
