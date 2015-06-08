7d53189a1d9f16e81bc323dc370722ff5df60ca2
NovikovViktor <viktorn@default-value.com>
2015-06-08 15:38:02 +0300

    Working on change html blocks strucuter and Css refactoring

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.getElementById('nav-button').onclick = function (event) {
     event.preventDefault();
     event.stopPropagation();
-    var nav_panel = document.getElementById('menu-nav').getElementsByClassName('nav')[0];
+    var nav_panel = document.getElementById('menu-nav');
     if (nav_panel.className.indexOf('visible-menu') > -1) {
         nav_panel.style.display = 'none';
         nav_panel.className = nav_panel.className.replace(' visible-menu', '');
