6ea8894986bd2306e6f76140ef7e4897cd81c683
NovikovViktor <viktorn@default-value.com>
2015-06-15 09:24:42 +0300

    Solve swiper js issue, rename classes etc.

--- js/swiper-slider.js
+++ js/swiper-slider.js
@@ -1,24 +1,16 @@
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3
 });
-
-if (screen.width <= 640) {
-    swiper.params.slidesPerView = 1;
-    swiper.update();
-} else {
-    swiper.params.slidesPerView = 3;
-    swiper.update();
-}
-
-window.onresize = function () {
-    var single_per = false;
-    if (screen.width <= 640 && single_per == false) {
-        swiper.params.slidesPerView = 1;
-        swiper.update();
-        single_per = true;
-    } else if (screen.width >= 640 && single_per == true) {
-        swiper.params.slidesPerView = 3;
-        swiper.update();
-        single_per = false;
+function sliderResize(slider) {
+    if (screen.width <= 640) {
+        slider.params.slidesPerView = 1;
+        slider.update();
+    } else if (screen.width >= 640) {
+        slider.params.slidesPerView = 3;
+        slider.update();
     }
+}
+sliderResize(swiper);
+window.onresize = function () {
+    sliderResize(swiper);
 };
